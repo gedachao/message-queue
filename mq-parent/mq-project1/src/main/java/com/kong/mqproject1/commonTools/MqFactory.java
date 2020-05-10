70de445b2712c64df4648391360975eed3d35a2d
gedachao07 <gedachao07@163.com>
2020-05-10 23:05:35 +0800

    add mq.properties files,compilable work in home or clc

--- mq-parent/mq-project1/src/main/java/com/kong/mqproject1/commonTools/MqFactory.java
+++ mq-parent/mq-project1/src/main/java/com/kong/mqproject1/commonTools/MqFactory.java
@@ -3,17 +3,30 @@ package com.kong.mqproject1.commonTools;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 /**
  * @author gedachao
  * @description
  * @date 2020/5/9 14:29
  */
 public class MqFactory {
-    private final static String url = "192.168.43.230";
+    private final static String resUrl = "mq/mq-home.properties";
     public static ConnectionFactory getConnectFactory(){
         ConnectionFactory connectionFactory = new ConnectionFactory();
-        connectionFactory.setHost(url);
-        connectionFactory.setPort(5672);
+        Properties properties = new Properties();
+        InputStream inputStream = MqFactory.class.getClassLoader().getResourceAsStream(resUrl);
+        try {
+            properties.load(inputStream);
+        } catch (IOException e) {
+            System.out.println("mq|mq-home.properties not found!");
+            e.printStackTrace();
+        }
+        System.out.println("host为"+properties.getProperty("mq.host"));
+        connectionFactory.setHost(properties.getProperty("mq.host"));
+        connectionFactory.setPort(Integer.valueOf(properties.getProperty("mq.port")));
         connectionFactory.setVirtualHost("/");
         return connectionFactory;
     }
